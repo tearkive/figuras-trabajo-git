1aab3dfcc808fc36b37b9a4296b4b48077b487b6
Ian-Rivera27 <riverafabian0227@gmail.com>
2024-09-12 11:25:41 -0600

    Cambios clase Cuadrado

--- Cuadrado.py
+++ Cuadrado.py
@@ -1,6 +1,7 @@
-Lado1 = int(input("Intruzca el primer lado \n"))
-Lado2 = int(input("Intruzca el segundo lado \n"))
+class Cuadrado:
+    Lado1 = int(input("Intruzca el primer lado \n"))
+    Lado2 = int(input("Intruzca el segundo lado \n"))
 
-Area = Lado1 * Lado2
+    Area = Lado1 * Lado2
 
-print("El area del cuadrado es: ", Area)
+    print("El area del cuadrado es: ", Area)
