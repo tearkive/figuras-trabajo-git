046254df7694217093f272088f82471ca47515f3
tearkive <evelyncorralejo2508@gmail.com>
2024-09-19 10:54:15 -0600

    Cambios a Circulo y main

--- Circulo.py
+++ Circulo.py
@@ -1,9 +1,8 @@
+import math 
+
 class Circulo:
     def __init__(self,radio):
         self.radio = radio
     
     def calcular_area(self):
-        return (self.radio)^2*3.1416
-
-#Ejemplo de uso
-circulo = Circulo(3)
+        return (math.pi)*(self.radio)**2
